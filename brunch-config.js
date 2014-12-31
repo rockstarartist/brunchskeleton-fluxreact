09a55a7407ca51436e9ec746787317a15f4bf4ac
Rock S. Artist <rockstarartist@gmail.com>
2014-12-31 12:22:45 -0500

    Corrected brunch aberrations, project is now served. Need to update jsx precompiler to compile react.js filenames. Also need to get the project functioning, probably a library problem.

--- brunch-config.js
+++ brunch-config.js
@@ -3,24 +3,26 @@ exports.config = {
         "stylesheets": {
             "defaultExtension": "css",
             "joinTo": {
-                "css/app.css": "/^app/",
-                "css/vendor.css": "/^vendor/"
-            },
-            "order": {
-                "before": ["vendor/styles/bootstrap.css"]
+                "css/app.css": /^app\/styles/,
+                "css/vendor.css": /^(bower_components|vendor)/
             }
         },
         "javascripts": {
-            "joinTo":{
-                "js/app.js": "^app\/actions|app\/components|app\/constants|app\/dispatcher|app\/stores|app",
-                "js/vendor.js": "^vendor",
-                "test/js/test.js": "^test(\/|\\)(?!vendor)",
-                "test/js/test-vendor.js": "^test(\/|\\)(?=vendor)"
+            "joinTo": {
+                "js/app.js": /^app/,
+                "js/vendor.js": /^(bower_components|vendor)/,
+                "test/js/test.js": /^test(\/|\\)(?!vendor)/,
+                "test/js/test-vendor.js": /^test(\/|\\)(?=vendor)/
             },
             "order": {
                 "before": [],
                 "after": []
             }
+        }
+    },
+    "plugins": {
+        "react": {
+            "harmony": true
         }
     }
 };
